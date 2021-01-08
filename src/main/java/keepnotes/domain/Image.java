13d298ee56a236310d234cde09100e806f228033
leks <kurbakov.av@gmail.com>
2021-01-09 02:06:18 +0300

    add: Image attachment

---
+++ src/main/java/keepnotes/domain/Image.java
@@ -0,0 +1,21 @@
+package keepnotes.domain;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Table(name = "images")
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class Image extends Attachment {
+
+    @NotEmpty
+    private Integer width;
+
+    @NotEmpty
+    private Integer height;
+}
