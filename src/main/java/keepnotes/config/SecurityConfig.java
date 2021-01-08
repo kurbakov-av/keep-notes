dc78124c1cea800c507b0f5c64f4d9aa3e9d7fbc
leks <kurbakov.av@gmail.com>
2021-01-08 18:53:16 +0300

    remove: unnecessary auth manager

--- src/main/java/keepnotes/config/SecurityConfig.java
+++ src/main/java/keepnotes/config/SecurityConfig.java
@@ -6,12 +6,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationEventPublisher;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.DefaultAuthenticationEventPublisher;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.user.OAuth2User;
@@ -19,7 +19,6 @@ import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.authentication.DelegatingAuthenticationEntryPoint;
 import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
@@ -31,9 +30,7 @@ import java.util.LinkedHashMap;
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final OAuth2UserService<OAuth2UserRequest, OAuth2User> userService;
-
-    private final AuthenticationManager authenticationManager;
+    private final OAuth2UserService<OAuth2UserRequest, OAuth2User> userService = new DefaultOAuth2UserService();
 
     @Bean
     public AuthenticationEventPublisher authenticationEventPublisher(ApplicationEventPublisher publisher) {
@@ -43,7 +40,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .addFilterAfter(new BasicAuthenticationFilter(authenticationManager), BasicAuthenticationFilter.class)
                 .exceptionHandling(handling -> handling.authenticationEntryPoint(entryPoint()))
                 .csrf(csrf -> csrf.ignoringAntMatchers("/api/**"))
                 .authorizeRequests(req -> req.mvcMatchers(HttpMethod.GET, "/api/**").permitAll())
