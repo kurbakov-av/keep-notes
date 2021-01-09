47b5f66576d2d49454fcd3dc324209f1fd6ca46f
leks <kurbakov.av@gmail.com>
2021-01-09 23:54:57 +0300

    add: User

---
+++ src/main/java/keepnotes/domain/User.java
@@ -0,0 +1,38 @@
+package keepnotes.domain;
+
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.time.LocalDateTime;
+import java.util.Collection;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty
+    @Column(unique = true)
+    private String username;
+
+    @NotEmpty
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "authorities")
+    private Collection<GrantedAuthority> authorities;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
